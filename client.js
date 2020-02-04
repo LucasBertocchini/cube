14581d16bfc4b3f7960adb845ed7b3c3f8e4796a
Lucas Bertocchini <lucasbertocchini@gmail.com>
2020-02-03 17:02:22 -0700

    display

--- client.js
+++ client.js
@@ -1,29 +1,59 @@
 "use strict";
 
 let cube = [
-    [ // white side
-        ["wbr", "wr", "wgr"],
-        ["wb", "w", "wg"],
-        ["wbo", "wo", "wgo"]
+    //     F     S      B     
+    [ //U
+        ["ybr", "yr", "ygr"], //R
+        ["yb" , "y" , "yg" ], //M
+        ["ybo", "yo", "ygo"]  //L
     ],
-    [
-        ["br", "r", "gr"],
-        ["b", "", "g"],
-        ["bo", "o", "go"]
+    [ //E
+        ["br" , "r" , "gr" ], //R
+        ["b"  , ""  , "g"  ], //M
+        ["bo" , "o" , "go" ]  //L
     ],
-    [ // yellow side
-        ["ybr", "yr", "ygr"],
-        ["yb", "y", "yg"],
-        ["ybo", "yo", "ygo"]
+    [ //D
+        ["wbr", "wr", "wgr"], //R
+        ["wb" , "w" , "wg" ], //M
+        ["wbo", "wo", "wgo"]  //L
     ]
 ]
 
 
 window.onload = () => {
-    console.table(cube);
+    
+}
+
+function turn(side) {
+    if (side === "U") {
+        [
+            [1,2,3],
+            [4,5,6],
+            [7,8,9]
+        ]
+        [
+            [3,6,9],
+            [2,5,8],
+            [1,4,7]
+        ]
+    }
 }
 
 
-function turn(side) {
-    
+function display() {
+    let html = "";
+    for (let x = 0; x < cube.length; x++) {
+        let plane = cube[x];
+        html += "<p>";
+        for (let y = 0; y < plane.length; y++) {
+            let line = plane[y];
+            for (let z = 0; z < line.length; z++) {
+                let piece = line[z];
+                html += (piece + " ");
+            }
+            html += "&nbsp;".repeat(20);
+        }
+        html += "</p>";
+    }
+    document.body.innerHTML = html;
 }
